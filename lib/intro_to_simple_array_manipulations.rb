e359ca6e80bb0182f5b72a9d845b0d719f703887
Kat B <katreesebarnes@gmail.com>
2019-02-09 16:01:57 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,14 @@ def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
     
 end
 
-def using_pop
+def using_pop(great_hits_of_the_nineties)
+  
+   great_hits_of_the_nineties.pop()
+  
+
+end
+
+def pop_with_args
   
   
 end
