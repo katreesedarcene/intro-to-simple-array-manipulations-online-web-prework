fa1e45f47fb19c4e42352d8cf3d705d61a0c0531
Kat B <katreesebarnes@gmail.com>
2019-02-09 04:20:14 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(countries_in_western_africa, next_country)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+ 
+  next_country = "Niger" 
+  
+  countries_in_western_africa.push("Niger")
+  
+end
+
+def using_unshift(neighborhoods_in_northwest_brooklyn, new_neighborhood)
+   neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+    new_neighborhood = "Brooklyn Heights"
+    
+    neighborhoods_in_northwest_brooklyn.unshift("Brooklyn Heights")
+    
+end
+
+def using_pop
+  
+  
+end
+
